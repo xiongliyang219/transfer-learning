a5ac9b5803a7e2f47790089584eae1d030b008d7
xly <xiongliyang219@gmail.com>
2017-02-15 21:43:13 -0800

    weight initialization and shuffle in fit function

--- tools/model.py
+++ tools/model.py
@@ -10,7 +10,7 @@ def build_transfer_net(output_dim, transfer_layer_name='fc2'):
 
     # Attach a softmax layer to the transfer layer as the new output layer.
     transfer_layer = base_model.get_layer(transfer_layer_name)
-    output_layer = Dense(output_dim, activation='softmax', name='predictions')
+    output_layer = Dense(output_dim, activation='softmax', name='predictions',init='he_normal')
     predictions = output_layer(transfer_layer.output)
     model = Model(input=base_model.input, output=predictions)
 
