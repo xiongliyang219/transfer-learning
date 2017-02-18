f3dca1f4097716fb52de94b08810286313b2c707
Bili Dong <qobilidop@gmail.com>
2017-02-18 15:35:11 -0800

    Introduce session

--- tools/training.py
+++ tools/training.py
@@ -1,7 +1,59 @@
 import pickle
 
+import h5py
+
 from tools import build_transfer_net
 from tools.datasets.urban_tribes import load_data
+
+
+class Session(object):
+    """Training session.
+
+    This is a helper class to bundle model and history together and make it
+    easy to resume training and recording history.
+    """
+    def __init__(self, model, history=None):
+        self.model = model
+        self.history = history
+
+    def dump(self, path):
+        W, b = self.model.layers[-1].get_weights()
+        with h5py.File(path, 'w') as f:
+            f.create_dataset('W', data=W)
+            f.create_dataset('b', data=b)
+            group = f.create_group('history')
+            for k, v in self.history.items():
+                group.create_dataset(k, data=v)
+
+    @classmethod
+    def load(cls, model, path):
+        with h5py.File(path, 'r') as f:
+            W = f['W'][:]
+            b = f['b'][:]
+            history = {}
+            for k, v in f['history'].items():
+                history[k] = list(v)
+        model.layers[-1].set_weights([W, b])
+        return cls(model, history=history)
+
+    def train(self, *args, **kwargs):
+        new_history = self.model.fit(*args, **kwargs)
+        self._record(new_history.history)
+
+    def _record(self, new_history):
+        if self.history is None:
+            self.history = new_history
+        else:
+            for key in self.history.keys():
+                self.history[key].extend(new_history[key])
+
+    @staticmethod
+    def _model_fpath(prefix):
+        return prefix + '.model.h5'
+
+    @staticmethod
+    def _history_fpath(prefix):
+        return prefix + '.history.json'
 
 
 def transfer_learn(layer_name, nb_sample, nb_epoch, output_file):
