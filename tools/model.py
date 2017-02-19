5a9ae9f8729f8d5b3d09e10bb9086b19b1e609f6
Bili Dong <qobilidop@gmail.com>
2017-02-19 10:22:32 -0800

    Add a flatten layer as needed

--- tools/model.py
+++ tools/model.py
@@ -1,5 +1,6 @@
 from keras.applications import VGG16
 from keras.layers import Dense
+from keras.layers import Flatten
 from keras.models import Model
 
 
@@ -8,11 +9,19 @@ def build_transfer_net(output_dim, transfer_layer_name='fc2'):
     # Start with the VGG16 model trained on the ImageNet dataset.
     base_model = VGG16(weights='imagenet')
 
-    # Attach a softmax layer to the transfer layer as the new output layer.
+    # Prepare a softmax layer to attach to transfer layer as the new output
+    # layer.
     transfer_layer = base_model.get_layer(transfer_layer_name)
+    transfer_output = transfer_layer.output
     output_layer = Dense(output_dim, init='he_normal', activation='softmax',
                          name='predictions')
-    predictions = output_layer(transfer_layer.output)
+
+    # Add a flatten layer as necessary.
+    if len(transfer_output.get_shape()) > 2:
+        transfer_output = Flatten()(transfer_output)
+
+    # Attach output layer to transfer layer.
+    predictions = output_layer(transfer_output)
     model = Model(input=base_model.input, output=predictions)
 
     # Ensure that only the last layer is going to be trained.
